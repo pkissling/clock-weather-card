c5c41b7fed3042f54339e04026f4dd960083a4f6
potrgani <drluka123456@gmail.com>
2023-04-27 13:05:45 +0200

    Update styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -52,7 +52,7 @@ export default css`
   clock-weather-card-today-right-wrap-center {
     display: flex;
     height: 4rem;
-    font-size: 8rem;
+    font-size: 6rem;
     white-space: nowrap;
     align-items: center;
     justify-content: center;
