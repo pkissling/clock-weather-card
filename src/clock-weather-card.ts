456ad38fa7cd6a3007f4d7bcc7afe46aa635d55e
potrgani <drluka123456@gmail.com>
2023-04-30 19:35:22 +0200

    Update clock-weather-card.ts

--- src/clock-weather-card.ts
+++ src/clock-weather-card.ts
@@ -1,77 +1,108 @@
-import { LitElement, html, css } from 'lit-element';
-import { HomeAssistant, WeatherEntity } from 'custom-card-helpers';
+import { LitElement, html, customElement, property } from 'lit-element';
 
-class ClockWeather extends LitElement {
-  static get properties() {
-    return {
-      hass: { type: Object },
-      weatherEntity: { type: Object },
-      timeFormat: { type: String },
-    };
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+}
+
+@customElement('clock-weather-card')
+export class ClockWeatherCard extends LitElement {
+
+  @property({ type: String }) apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+  @property({ type: String }) location = 'New York';
+  @property({ type: Number }) updateInterval = 300000; // 5 minutes
+
+  private timeInterval: NodeJS.Timeout;
+  private weatherData: WeatherData;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.getTime();
+    this.getWeather();
+    this.timeInterval = setInterval(() => {
+      this.getTime();
+      this.getWeather();
+    }, this.updateInterval);
   }
 
-  static get styles() {
-    return css`
-      .clock {
-        font-size: 2rem;
-        font-weight: bold;
-      }
-
-      .weather {
-        font-size: 1.5rem;
-        margin-top: 0.5rem;
-      }
-    `;
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.timeInterval);
   }
 
-  constructor() {
-    super();
-    this.weatherEntity = null;
-    this.timeFormat = 'h:mm A';
+  async getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&units=metric&appid=${this.apiKey}`);
+    if (response.ok) {
+      const data = await response.json();
+      this.weatherData = {
+        name: data.name,
+        main: data.main,
+        weather: data.weather
+      };
+      this.requestUpdate();
+    } else {
+      console.error(`Error fetching weather data: ${response.statusText}`);
+    }
+  }
+
+  getTime() {
+    const now = new Date();
+    this.shadowRoot!.getElementById('clock')!.textContent = now.toLocaleTimeString();
   }
 
   render() {
     return html`
-      <div class="clock">${this._getTime()}</div>
-      ${this.weatherEntity
-        ? html`<div class="weather">${this._getWeather()}</div>`
-        : null}
+      <style>
+        .card {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          width: 200px;
+          height: 200px;
+          background-color: #fff;
+          border-radius: 10px;
+          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+          font-family: Arial, sans-serif;
+        }
+        .card__time {
+          font-size: 24px;
+          margin-bottom: 16px;
+        }
+        .card__location {
+          font-size: 18px;
+          margin-bottom: 8px;
+        }
+        .card__icon {
+          width: 64px;
+          height: 64px;
+          margin-bottom: 8px;
+        }
+        .card__temp {
+          font-size: 24px;
+          margin-bottom: 8px;
+        }
+        .card__description {
+          font-size: 16px;
+        }
+      </style>
+      <div class="card">
+        <div class="card__time" id="clock"></div>
+        ${this.weatherData ? html`
+          <div class="card__location">${this.weatherData.name}</div>
+          <img class="card__icon" src="https://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png" alt="${this.weatherData.weather[0].description}">
+          <div class="card__temp">${this.weatherData.main.temp.toFixed(1)}&deg;C</div>
+          <div class="card__description">${this.weatherData.weather[0].description}</div>
+        ` : ''}
+      </div>
     `;
   }
 
-  setConfig(config) {
-    if (!config.weather_entity) {
-      throw new Error('Please provide a weather entity');
-    }
-    this.weatherEntity = config.weather_entity;
-    if (config.time_format) {
-      this.timeFormat = config.time_format;
-    }
-  }
-
-  set hass(hass: HomeAssistant) {
-    this._hass = hass;
-    if (this.weatherEntity) {
-      this.weatherEntity = hass.states[this.weatherEntity];
-    }
-  }
-
-  _getTime() {
-    const now = new Date();
-    const formatter = new Intl.DateTimeFormat(this._hass.locale, {
-      hour: this.timeFormat.includes('h') ? 'numeric' : undefined,
-      minute: 'numeric',
-      hour12: this.timeFormat.includes('A'),
-    });
-    return formatter.format(now);
-  }
-
-  _getWeather() {
-    const stateObj = this._hass.states[this.weatherEntity];
-    const weatherState = stateObj.state;
-    const temperature = stateObj.attributes.temperature;
-    return `${weatherState} ${temperature}°C`;
-  }
 }
-
-customElements.define('clock-weather', ClockWeather);
