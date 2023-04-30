8f8acfa396aed368d8b536aade715f0ae80854ae
potrgani <drluka123456@gmail.com>
2023-04-30 20:15:04 +0200

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,16 +1,14 @@
 import { html, TemplateResult } from "lit";
 
-export async function safeRender<T>(renderFn: () => T): Promise<T | TemplateResult> {
-  let numRetries = 3;
-  while (numRetries > 0) {
+export function safeRender<T>(renderFn: () => T): T | TemplateResult {
+ let retries = 3
+  while (retries > 0) {
     try {
-      return renderFn();
+      return renderFn()
     } catch (e) {
-      if (--numRetries === 0) {
-        console.error('clock-weather-card - Error while rendering clock-weather-card component:', e);
-        return html``;
-      }
+      console.error('clock-weather-card - Error while rendering clock-weather-card component:', e)
+      retries--
     }
-    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second before retrying
   }
+  return html``
 }
