80345a3db47f95c0066a696d1befffa33e099f30
Patrick Kissling <patrick.kissling@de.bosch.com>
2023-10-06 10:18:56 +0200

    Configure eslint properly

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,33 @@
-export function max(n: number[]): number {
-  return Math.max(...n);
+export function max (n: number[]): number {
+  return Math.max(...n)
 }
 
-export function min(n: number[]): number {
-  return Math.min(...n);
+export function min (n: number[]): number {
+  return Math.min(...n)
 }
 
-export function round(n: number, precision = 0): number {
+export function round (n: number, precision = 0): number {
   if (precision <= 0) {
-    return Math.round(n);
+    return Math.round(n)
   }
-  return Math.ceil((n - precision / 2) / precision) * precision;
+  return Math.ceil((n - precision / 2) / precision) * precision
 }
 
-export function roundUp(n: number, precision = 0): number {
-  if (!precision) {
-    return Math.ceil(n);
+export function roundUp (n: number, precision: number = 0): number {
+  if (precision <= 0) {
+    return Math.ceil(n)
   }
-  return Math.ceil(n / precision) * precision;
+  return Math.ceil(n / precision) * precision
 }
 
-export function roundDown(n: number, precision = 0): number {
-  if (!precision) {
-    return Math.floor(n);
+export function roundDown (n: number, precision = 0): number {
+  if (precision <= 0) {
+    return Math.floor(n)
   }
-  return Math.floor(n / precision) * precision;
+  return Math.floor(n / precision) * precision
 }
 
-export function roundIfNotNull(number: number | null): number | null {
+export function roundIfNotNull (number: number | null): number | null {
   if (number === null) {
     return null
   }
@@ -35,19 +35,22 @@ export function roundIfNotNull(number: number | null): number | null {
   return Math.round(number)
 }
 // from https://stackoverflow.com/a/1053865
-export function extractMostOccuring<T extends string | number | symbol>(elements: T[]): T {
-  const modeMap = {} as Record<T, number>; let maxEl = elements[0], maxCount = 1;
+export function extractMostOccuring<T extends string | number | symbol> (elements: T[]): T {
+  const modeMap = new Map<T, number>()
+  let maxEl = elements[0]
+  let maxCount = 1
   for (let i = 0; i < elements.length; i++) {
-    const el = elements[i];
-    if (modeMap[el] == null) {
-      modeMap[el] = 1;
+    const el = elements[i]
+    if (modeMap.get(el) === undefined) {
+      modeMap.set(el, 1)
     } else {
-      modeMap[el]++;
-      if (modeMap[el] > maxCount) {
-        maxEl = el;
-        maxCount = modeMap[el];
+      const n = modeMap.get(el) ?? 0
+      modeMap.set(el, n + 1)
+      if ((modeMap.get(el) ?? 0) > maxCount) {
+        maxEl = el
+        maxCount = modeMap.get(el) ?? 0
       }
     }
   }
-  return maxEl;
+  return maxEl
 }
