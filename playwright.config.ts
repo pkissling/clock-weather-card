55199f9a622cb1330b7ee58f52d5d8f48e780e8f
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-09-11 21:55:32 +0200

    Create OS-agnostic playwright snapshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './e2e',
+  /* Use OS-agnostic snapshot filenames: {arg}-{projectName}.png */
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
