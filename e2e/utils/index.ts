8d7a6266001c5b40107a20c887a991c3c0b61704
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-09-12 11:06:08 +0200

    Increase frequency of snapshot polling

--- e2e/utils/index.ts
+++ e2e/utils/index.ts
@@ -1,8 +1,10 @@
 import { expect, Page } from '@playwright/test'
 
-export const expectScreenshot = async (page: Page, name: string): Promise<void> => {
+export const expectScreenshot = async (page: Page, screenshotName: string): Promise<void> => {
   const clockWeatherCard = page.locator('clock-weather-card-dev')
+
   await expect(async () => {
-    await expect(clockWeatherCard).toHaveScreenshot(name, { maxDiffPixelRatio: 0.003 })
-  }).toPass({ timeout: 5_000 })
+    console.log('Taking screenshot for weather entity test', new Date().toISOString())
+    await expect(clockWeatherCard).toHaveScreenshot(screenshotName, { maxDiffPixelRatio: 0.01 })
+  }).toPass({ intervals: [1] })
 }
