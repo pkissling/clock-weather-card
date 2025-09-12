57a74e29da2803fb2be553677eceffa1e415d2eb
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-09-12 14:23:59 +0200

    Always run 2 retries in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* No retries required */
-  retries: 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Timeout for each test. */
