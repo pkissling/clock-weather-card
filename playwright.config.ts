5f7f7ce304244cb8985e3053e3c106552c5444cd
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-09-12 11:06:08 +0200

    Increase frequency of snapshot polling

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,14 +17,14 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* No retries required */
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Timeout for each test. */
-  timeout: 5000,
+  timeout: 10_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
