a0ae884f9925467e48dfb3653feef004b665e393
perceival <perceivalpercy@gmail.com>
2026-03-18 07:22:08 +0100

    Make PR 616 display changes optional and document config

--- src/types.ts
+++ src/types.ts
@@ -37,7 +37,11 @@ export interface ClockWeatherCardConfig extends LovelaceCardConfig {
   apparent_sensor?: string
   aqi_sensor?: string
   cycle_display?: number; // 0 disables cycling, otherwise specifies seconds for cycling
+  outdoor_temp_sensor?: string
   displayed_temperature?: 'current' | 'apparent' | 'outdoor';
+  large_display?: boolean
+  today_icon_scale?: number
+  today_font_scale?: number
 }
 
 export interface MergedClockWeatherCardConfig extends LovelaceCardConfig {
@@ -64,6 +68,12 @@ export interface MergedClockWeatherCardConfig extends LovelaceCardConfig {
   show_decimal: boolean
   apparent_sensor?: string
   aqi_sensor?: string
+  cycle_display: number
+  outdoor_temp_sensor?: string
+  displayed_temperature: 'current' | 'apparent' | 'outdoor'
+  large_display: boolean
+  today_icon_scale: number
+  today_font_scale: number
 }
 
 export const enum WeatherEntityFeature {
